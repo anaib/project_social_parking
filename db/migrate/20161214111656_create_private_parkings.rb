ac93f442d7198c6644adb320766e425f3f396e4c
Raghu Betina <raghu@firstdraft.com>
2016-12-14 11:16:56 +0000

    Generate private_parkings resource with starter:resource

---
+++ db/migrate/20161214111656_create_private_parkings.rb
@@ -0,0 +1,13 @@
+class CreatePrivateParkings < ActiveRecord::Migration
+  def change
+    create_table :private_parkings do |t|
+      t.integer :offeree_user_id
+      t.integer :acceptee_user_id
+      t.datetime :date
+      t.time :time
+
+      t.timestamps
+
+    end
+  end
+end
